f9bcda13c7fa6c1739b928e3326ec34b98064d0a
Santiago <santiago.rivas@campusucc.edu.co>
2026-05-28 00:31:47 -0500

    EXPORTADOR

--- src/main/java/co/studyflow/patterns/factory/ExportadorJSON.java
+++ src/main/java/co/studyflow/patterns/factory/ExportadorJSON.java
@@ -5,19 +5,25 @@ import com.google.gson.GsonBuilder;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.List;
 import co.studyflow.model.Tarjeta;
+import co.studyflow.util.LocalDateTimeAdapter;
 
 /**
  * Exportador JSON - implementación del Abstract Factory
  */
 public class ExportadorJSON implements Exportador {
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .create();
     
     @Override
     public void exportar(List<Tarjeta> tarjetas, OutputStream output) throws IOException {
         String json = gson.toJson(tarjetas);
-        try (OutputStreamWriter writer = new OutputStreamWriter(output)) {
+        try (OutputStreamWriter writer = new OutputStreamWriter(output, StandardCharsets.UTF_8)) {
             writer.write(json);
             writer.flush();
         }
